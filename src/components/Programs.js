c8789181683724577a26d1c27307d67d6ae81b39
Hannah Stahl <hls62@cornell.edu>
2020-06-18 14:45:42 -0500

    Styled Programs page

--- src/components/Programs.js
+++ src/components/Programs.js
@@ -2,24 +2,27 @@ import React from 'react';
 import config from '../config';
 
 const Programs = ({ programs }) => (
-  <div>
-    <h1>Our Programs</h1>
+  <div className="programs-page-content">
+    <h1>
+      Help your workplace learn about diversity and inclusion through convenient online courses.
+    </h1>
     <div className="programs">
       {programs.map((program) => (
-        <a
-          key={program.itemId}
-          href={program.link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="program"
-        >
+        <div key={program.itemId} className="program">
           <img
             src={`${config.cloudfrontURL}/${program.photos[0].photoName}`}
             alt={program.itemName}
             className="program-img"
           />
-          <h3>{program.itemName}</h3>
-        </a>
+          <div className="program-text">
+            <h3>{program.itemName}</h3>
+            <p>{program.itemDescription}</p>
+            <a href={program.itemLink} target="_blank" rel="noopener noreferrer">
+              Sign up for the course
+              <i className="fas fa-external-link-alt" />
+            </a>
+          </div>
+        </div>
       ))}
     </div>
   </div>
