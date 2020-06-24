68b180c59ef399347424dbcd5fe837960029c49d
Hannah Stahl <hls62@cornell.edu>
2020-06-24 18:27:02 -0500

    Added real text and headers to About page

--- src/components/About.js
+++ src/components/About.js
@@ -4,15 +4,19 @@ import config from '../config';
 
 const About = () => (
   <div className="about-page-content">
-    {content.bio.map((paragraph, index) => (
-      <div key={paragraph} className="about-page-content-block">
-        {(index % 2 === 0 || window.innerWidth <= 957) && (
-          <img src={`${config.publicCloudfrontURL}/agbanobi-about-${index + 1}.jpg`} alt={config.businessName} />
-        )}
-        <div className="about-text"><p>{paragraph}</p></div>
-        {(index % 2 === 1 && window.innerWidth > 957) && (
-          <img src={`${config.publicCloudfrontURL}/agbanobi-about-${index + 1}.jpg`} alt={config.businessName} />
-        )}
+    {content.about.map((paragraph, index) => (
+      <div key={paragraph.header} className="about-page-section">
+        <h1 className="about-page-section-header">{paragraph.header}</h1>
+        <div className="about-page-content-block">
+          {(index % 2 === 0 || window.innerWidth <= 957) && (
+            <img src={`${config.publicCloudfrontURL}/agbanobi-about-${index + 1}.jpg`} alt={config.businessName} />
+          )}
+          <div className="about-text"><p>{paragraph.text}</p></div>
+          {(index % 2 === 1 && window.innerWidth > 957) && (
+            <img src={`${config.publicCloudfrontURL}/agbanobi-about-${index + 1}.jpg`} alt={config.businessName} />
+          )}
+        </div>
+        {index < 2 && <hr />}
       </div>
     ))}
   </div>
