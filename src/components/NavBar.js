c4e9930092993f8dc33872a81f9b6a1c15bfb3e3
Hannah Stahl <hls62@cornell.edu>
2020-07-12 09:58:19 -0500

    Added different logo resolutions for different screens

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 
+const getLogo = (devicePixelRatio) => {
+  if (devicePixelRatio === 3) return 'logo_3x.png';
+  if (devicePixelRatio === 2) return 'logo_2x.png';
+  return 'logo.png';
+};
+
 const NavBar = () => (
   <Navbar expand="lg" position="top">
-    <img src="logo.png" alt="Home" className="logo" />
+    <img src={getLogo(window.devicePixelRatio)} alt="Home" className="logo" />
   </Navbar>
 );
 
